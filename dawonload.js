60c349e90cccc775aa750c11b35eebf8cee79093
MunirAlFaris <149905337+MunirAlFaris@users.noreply.github.com>
2023-12-03 13:25:34 +0300

    starting

---
+++ dawonload.js
@@ -0,0 +1,29 @@
+let botton = document.querySelector(".d-button");
+let link = document.querySelector("a");
+let span = document.querySelector(".prog");
+let spanProg = document.querySelector(".prog > span");
+let thankMsg = document.querySelector(".thank");
+botton.onclick = () => {
+  span.classList.add("active");
+  console.log("true");
+  let width = 0;
+  let count = setInterval(() => {
+    width++;
+    spanProg.style.width = `${width}%`;
+    if (width < 30) {
+      spanProg.style.backgroundColor = "#EE5622";
+    } else if (width > 30 && width < 70) {
+      spanProg.style.backgroundColor = "#ECA72C";
+    } else {
+      link.click();
+      spanProg.style.backgroundColor = "#56c553";
+    }
+    if (width === 100) {
+      botton.classList.add("done");
+      botton.innerText = "لقد بدأ التحميل";
+      thankMsg.classList.add("active");
+      span.style.display = "none";
+      clearInterval(count);
+    }
+  }, 70);
+};
